9c82abe799b3b1adf6d4a967221c7928194d64b5
sarah.evans <sarah.evans@MU-C1-01.mullen.pvt.k12.co.us>
2019-08-26 11:14:17 -0600

    rolling again

--- Craps Simulation/src/crapsSimulation.java
+++ Craps Simulation/src/crapsSimulation.java
@@ -9,9 +9,35 @@ public class crapsSimulation
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Hello! What is your name?");
 		String userName = userInput.nextLine();
-		System.out.println("Nice to meet you " + userName + "! Ready for a great game of Craps?");
+		System.out.println("Nice to meet you " + userName + "! Ready for a great game of Craps? Press enter to start!");
+		String dummy = userInput.nextLine();
+		boolean isStillRolling = true;
+		boolean isStillRolling1 = true;
+		while (isStillRolling = true)
+		{
 		int dice1 = (int)(Math.random()* 7) + 1; 
-
+		int dice2 = (int)(Math.random()* 7) + 1; 
+		int totalDice = dice1 + dice2;
+		System.out.println("You rolled a " + dice1 + " and a " + dice2 + " for a total of " + totalDice);
+		if (totalDice == 2 || totalDice == 12)
+		{
+			System.out.println("You rolled a " + dice1 + " and a " + dice2 + " for a total of " + totalDice + ". You lose, " + userName + ".");
+			isStillRolling = false;
+		}
+		
+		if (totalDice == 7 || totalDice == 11)
+		{
+			System.out.println("You rolled a " + dice1 + " and a " + dice2 + " for a total of " + totalDice + ". You win, " + userName + "!");
+			isStillRolling = false;
+		}
+		
+		while (isStillRolling1 = true)
+		{ 
+			System.out.println("Your point is: " + totalDice + ", " + userName + ". Press enter to roll again!");
+			String dummy1 = userInput.nextLine();
+			isStillRolling = true; 		
+		}
 	}
+}
 
 }
